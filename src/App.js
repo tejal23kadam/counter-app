8c71390c6a460d43280d26b4ce0af68dc506547a
tejal23kadam <154776489+tejal23kadam@users.noreply.github.com>
2024-07-29 10:55:21 +0530

    added code for increment decrement count

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let[digit,updateDigit] = useState(0);
+  
+  const increment = () =>{
+    if(digit >=0)
+      updateDigit(digit+1);    
+  }
+  const decrement = () =>{
+    if(digit >=1)
+      updateDigit(digit-1);   
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="myClass">
+      <div>
+        <h1>{digit}</h1>
+      </div>
+      <div>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
+      </div>
+
     </div>
   );
 }
